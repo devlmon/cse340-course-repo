73367c8198daebee8a9a42b332865d3624c76fed
devlmon <lmontenegrollamozas@byupathway.edu>
2026-07-13 20:26:00 +0200

    Minor Fix: self-signed certificate

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false // set true if not using self‑signed certificates
+    }
 });
 
 /**
